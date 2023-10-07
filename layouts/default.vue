9093d0810112b5ac1ab83a316d335cc2e6248064
tokuyama_shota <tokush1031@gmail.com>
2023-10-07 16:46:10 +0900

    Vutify導入からtheme機能追加動作確認

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+
+const darkTheme = ref(false);
+const theme = useTheme();
+
+const changeTheme = () => {
+  theme.global.name.value = darkTheme.value ? "mainDarkTheme" : "mainTheme";
+};
+</script>
+
+<template>
+  <v-app-bar>
+    <v-app-bar-title> Nuxt3 Vutify3 </v-app-bar-title>
+    <template v-slot:append>
+      <v-switch
+        v-model="darkTheme"
+        @update:model-value="changeTheme"
+        hide-details
+        inset
+        class="mr-auto"
+      />
+    </template>
+  </v-app-bar>
+  <v-main><slot /></v-main>
+</template>
